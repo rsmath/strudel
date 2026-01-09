5b046006203df9c8c8b68f6a9323b7f78f23d553
Ramansh Sharma <ramansh@cs.utah.edu>
2026-01-09 10:25:40 -0700

    asdf

--- drum_beats.cpp
+++ drum_beats.cpp
@@ -5,6 +5,15 @@
 
    RolandTR909, ViscoSpaceDrum
    viscospacedrum_bd(1)
+   
+   claps
+   bossdr110
+   bossdr550
+   alesishr16
+   alesissr16
+   casiorz1
+   circuitsdrumtracks
+   cp(2)
 */
 
 sound("<bd bd sd lt bd lt bd lt lt lt lt bd bd sd>*10").bank("ViscoSpaceDrum")
@@ -50,4 +59,54 @@ stack(a);
 
 setcpm(100/4)
 const a = s("mt*2 - sd mt - mt*2 sd").bank("ViscoSpaceDrum");
-stack(a);
+const b = s("cp:1 -");
+/* const c = s("cp").bank("casiorz1"); */
+const c = s("cp").gain(0.5).bank("claps");
+stack(a, c);
+
+setcpm(120/4)
+const a = s("bd mt*2 - [hh oh]").bank("ViscoSpaceDrum, RolandTR909");
+const b = s("- - cp:1 -");
+stack(a, b)._punchcard();
+
+setcpm(120/4)
+const a = s("[bd*2 -] mt*2 - [hh oh]").bank("ViscoSpaceDrum, RolandTR909");
+const b = s("- - cp:1 -");
+stack(a, b)._punchcard({cycles: 4, labels: 1});
+
+$: note("<[c2 c3]*4 [bb1 bb2]*4 [f2 f3]*4 [eb2 eb3]*4>")
+.sound("gm_synth_bass_1").lpf(800)
+$: n(`<
+[~ 0] 2 [0 2] [~ 2]
+[~ 0] 1 [0 1] [~ 1]
+[~ 0] 3 [0 3] [~ 3]
+[~ 0] 2 [0 2] [~ 2]
+>*4`).scale("C4:minor")
+.sound("gm_synth_strings_1")
+$: sound("bd*4, [~ <sd cp>]*2, [~ hh]*4")
+.bank("RolandTR909")
+
+/*
+
+gm_electric_bass_finger(1)
+gm_synth_bass_1
+gm_fretless_bass_2
+
+*/
+// lets make a badass beat
+setcpm(120/4)
+
+$: note("<[c2 c3]*4 [bb1 bb2]*4 [f2 f3]*4 [eb2 eb3]*4>")
+.sound("<gm_electric_bass_finger gm_synth_bass_1>").lpf(rand.range(1000, 1600))
+
+_$: n(`<
+[~ 0] 2 [0 2] [~ 2]
+[~ 0] 1 [0 1] [~ 1]
+[~ 0] 3 [0 3] [~ 3]
+[~ 0] 2 [0 2] [~ 2]
+>*4`).scale("C4:minor")
+.sound("gm_synth_strings_1")
+
+_$: sound("bd*4, [~ <sd cp>]*2, [~ hh]*4")
+.bank("RolandTR909")
+
